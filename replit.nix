c1d8795dbb4329626c0bbc5daf85fd19900df4d7
Mattsantos541 <50892116+Mattsantos541@users.noreply.github.com>
2025-02-13 21:16:32 +0000

    updated .replit file

--- replit.nix
+++ replit.nix
@@ -1,6 +1,13 @@
-{pkgs}: {
-  deps = [
-    pkgs.psmisc
-    pkgs.lsof
-  ];
+{ pkgs }: {
+    deps = [
+        pkgs.python310
+        pkgs.uvicorn
+        pkgs.python310Packages.fastapi
+        pkgs.python310Packages.requests
+        pkgs.python310Packages.sanic
+        pkgs.python310Packages.pydantic
+        pkgs.python310Packages.sqlalchemy
+    ];
+    # Run backend automatically
+    run = "uvicorn main:app --host 0.0.0.0 --port 9000 --reload";
 }
