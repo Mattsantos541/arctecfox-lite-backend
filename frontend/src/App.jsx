63574ffa7b327b37419f5df3dbeb8c9ba8c7ca71
Mattsantos541 <50892116+Mattsantos541@users.noreply.github.com>
2025-02-25 03:34:09 +0000

    company overview displaying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import CompanyOverview from "./pages/CompanyOverview"; // Updated name
+import CompanyOverview from "./pages/CompanyOverview";
 import Login from "./pages/Login";
 import PMPlanner from "./pages/PMPlanner";
 import WorkOrders from "./pages/WorkOrders";
-import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
 
 function App() {
     return (
         <Router>
-            <MainLayout>
-                <Routes>
-                    <Route path="/" element={<CompanyOverview />} /> {/* Default to Company Overview */}
-                    <Route path="/login" element={<Login />} />
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<CompanyOverview />} /> {/* ✅ Default to Company Overview */}
                     <Route path="/company-overview" element={<CompanyOverview />} />
                     <Route path="/pm-planner" element={<PMPlanner />} />
                     <Route path="/work-orders" element={<WorkOrders />} />
-                    <Route path="/reports" element={<Reports />} />
                     <Route path="/settings" element={<Settings />} />
-                </Routes>
-            </MainLayout>
+                </Route>
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </Router>
     );
 }
