2389b0682ee78fb6d4c60e1622e2df163e45a2ce
Mattsantos541 <50892116+Mattsantos541@users.noreply.github.com>
2025-04-05 14:10:41 +0000

    deleted frontend pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,34 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import MainLayout from "./layouts/MainLayout";
-import CompanyOverview from "./pages/CompanyOverview";
-import Login from "./pages/Login";
-import CompleteProfile from "./pages/CompleteProfile";
 import PMPlanner from "./pages/PMPlanner";
-import WorkOrders from "./pages/WorkOrders";
-import Reports from "./pages/Reports";
-import Settings from "./pages/Settings";
-import { AuthProvider } from "./hooks/useAuth";
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/complete-profile" element={<CompleteProfile />} />
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<CompanyOverview />} />
-            <Route path="/company-overview" element={<CompanyOverview />} />
-            <Route path="/pm-planner" element={<PMPlanner />} />
-            <Route path="/work-orders" element={<WorkOrders />} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/settings" element={<Settings />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<PMPlanner />} />
+      </Routes>
     </Router>
   );
 }
